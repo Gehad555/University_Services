70ec770a06aeaf341af4561bb31df5e5a6c212f2
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2023-09-21 17:21:58 +0300

    semi login 😂
    semi login 😂

---
+++ Controllers/authController.js
@@ -0,0 +1,22 @@
+const userModel = require('../Models/userModel');
+
+// login controller
+const loginController = async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+        if(!userName || !password) {
+            return res.status(400).json({ msg: 'Please enter all fields' });   
+        }
+        // check User
+        const user = await userModel.findOne({ userName });
+        if(!user) {
+            return res.status(400).json({ msg: 'User does not exist' });
+        }
+    
+    }
+    catch(err) {
+        return res.status(500).json({ msg: err.message });
+    }
+};
+
+module.exports={loginController};
