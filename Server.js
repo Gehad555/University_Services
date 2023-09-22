72f5918f4a94a34eaf23234e398dae450e449292
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-22 20:53:50 +0300

    add front

--- Server.js
+++ Server.js
@@ -1,25 +1,21 @@
-require('./Config/db')
-const express = require('express');
-const dotenv = require('dotenv');
-const authRouter = require('./routes/authRoute');
-const mongoose = require('mongoose');
+require("./Config/db");
+const express = require("express");
+const dotenv = require("dotenv");
+const authRouter = require("./routes/authRoute");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
-
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/auth", authRouter);
 
-app.get('/',(req,res)=>{
-    res.send('Hello World !')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World !");
+});
 
-
-
-app.listen(port,()=>{
-    console.log(`University app listening at http://localhost:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`University app listening at http://localhost:${port}`);
+});
