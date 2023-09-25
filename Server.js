46c3116e7d7b4387bb519713d91a37851e06e68f
Yasmeen Fakhri <yasmeenayr@gmail.com>
2023-09-25 15:22:00 +0200

    connect database in atlas

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
-require('./Config/db')
+require('dotenv').config();
+//require('./Config/db')
 const express = require('express');
 const dotenv = require('dotenv');
 const authRouter = require('./routes/authRoute');
@@ -13,13 +14,16 @@ const app = express();
 app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+        .then(()=> {
+                app.listen(port,()=>{
+                                    console.log(`University app listening at http://localhost:${port}`);
+                                    })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 app.get('/',(req,res)=>{
     res.send('Hello World !')
 })
-
-
-
-app.listen(port,()=>{
-    console.log(`University app listening at http://localhost:${port}`);
-})
-
