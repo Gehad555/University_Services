e8d8cb6cfc853bb15ec22e3b7ccb1dafda49fb9f
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-10-04 02:13:52 +0300

    edit nav

--- Client/src/component/Home.jsx
+++ Client/src/component/Home.jsx
@@ -5,14 +5,57 @@ const Home = () => {
   return (
     <>
       <div className={style.home} style={{ height: "100vh" }}>
-        <nav className="navbar sticky-top bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid" style={{ padding: "0 50px" }}>
+            <img
+              src="faculty-image-removebg-preview.png"
+              alt=""
+              width={"100px"}
+              style={{ padding: "0 15px" }}
+            />
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+              <div
+                id={style.linkId}
+                className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-5 text-center"
+              >
+                <Link>Home</Link>
+                <Link>About Us</Link>
+                <Link>About college</Link>
+                <Link>College Activities</Link>
+              </div>
+              <div className={style.inputBox_container}>
+                <svg
+                  className={style.search_icon}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 48 48"
+                  alt="search icon"
+                >
+                  <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
+                </svg>
+                <input
+                  className={style.inputBox}
+                  id="inputBox"
+                  type="text"
+                  placeholder="Search..."
+                />
+              </div>
+            </div>
+          </div>
+        </nav>
+        {/* <nav className="navbar sticky-top bg-body-tertiary">
           <div className="container-fluid justify-content-around">
             <img
               src="faculty-image-removebg-preview.png"
               alt=""
               width={"100px"}
             />
-            <div id={style.linkId} className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-4">
+            <div
+              id={style.linkId}
+              className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-4"
+            >
               <Link>Home</Link>
               <Link>About Us</Link>
               <Link>About college</Link>
@@ -35,15 +78,14 @@ const Home = () => {
               />
             </div>
           </div>
-        </nav>
+        </nav> */}
         <div className={style.data}>
           <h2>Welcome suez canal university </h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit,
             facilis. Aliquam reiciendis, debitis consequatur vitae repellat
             voluptates voluptate ipsa perferendis voluptas ullam praesentium
-            tenetur perspiciatis beatae itaque adipisci et facilis.
-            lore
+            tenetur perspiciatis beatae itaque adipisci et facilis. lore
           </p>
           <div className="d-flex align-items-center justify-content-center gap-5 text-center">
             <Link className="px-5">Login</Link>
@@ -51,8 +93,91 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="">
-
+      <div className={style.content}>
+        <div className="d-flex flex-column align-items-center" style={{ gap: "40px" }}>
+          <h3 className="pt-5">College departments</h3>
+          <div className="w-50 p-3 rounded-4" style={{ border: "2px solid #57C6F2" }}>
+            <h4 className="text-center py-3">Information System</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              assumenda iste impedit, ut harum eius doloribus ullam perferendis,
+              adipisci necessitatibus voluptas, voluptates maiores quasi quam
+              modi rerum non? Voluptatum, provident.
+            </p>
+          </div>
+          <div className="w-50 p-3 rounded-4" style={{ border: "2px solid #57C6F2" }}>
+            <h4 className="text-center py-3">Computer Science </h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              assumenda iste impedit, ut harum eius doloribus ullam perferendis,
+              adipisci necessitatibus voluptas, voluptates maiores quasi quam
+              modi rerum non? Voluptatum, provident.
+            </p>
+          </div>
+          <div className="w-50 p-3 rounded-4" style={{ border: "2px solid #57C6F2" }}>
+            <h4 className="text-center py-3">Artificial intelligence</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              assumenda iste impedit, ut harum eius doloribus ullam perferendis,
+              adipisci necessitatibus voluptas, voluptates maiores quasi quam
+              modi rerum non? Voluptatum, provident.
+            </p>
+          </div>
+          <div className="w-50 p-3 rounded-4" style={{ border: "2px solid #57C6F2" }}>
+            <h4 className="text-center py-3">software</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              assumenda iste impedit, ut harum eius doloribus ullam perferendis,
+              adipisci necessitatibus voluptas, voluptates maiores quasi quam
+              modi rerum non? Voluptatum, provident.
+            </p>
+          </div>
+        </div>
+        <div className="d-flex border-top mt-5 border-black">
+          <div className="im">
+            <img src="faculty-image-removebg-preview.png" alt="" />
+          </div>
+          <div className="m">
+            <p>about us</p>
+            <div className="d">
+              <i className="fa-brands fa-linkedin"></i>
+              <p>(6) Ehab Yousef | LinkedIn</p>
+            </div>
+            <div className="d">
+              <i className="fa-brands fa-linkedin"></i>
+              <p>(6) Ehab Yousef | LinkedIn</p>
+            </div>
+            <div className="d">
+              <i className="fa-brands fa-linkedin"></i>
+              <p>(6) Ehab Yousef | LinkedIn</p>
+            </div>
+            <div className="d">
+              <i className="fa-brands fa-linkedin"></i>
+              <p>(6) Ehab Yousef | LinkedIn</p>
+            </div>
+          </div>
+          <div className="e">
+            <p>About College</p>
+            <div className="d">
+              <i className="fa-solid fa-phone"></i>
+              <p>(+2064) 3223007-(+2064) 32001258</p>
+            </div>
+            <div className="d">
+              <i className="fa-regular fa-envelope"></i>
+              <p>President-office@suez.edu.eg</p>
+            </div>
+            <div className="d">
+              <i className="fa-solid fa-location-dot"></i>
+              <p>4.5 Km the Ring Road, Ismailia, Egypt</p>
+            </div>
+            <div className="d">
+              <i className="fa-brands fa-facebook-f"></i>
+              <i className="fa-brands fa-instagram"></i>
+              <i className="fa-brands fa-x-twitter"></i>
+              <i className="fa-brands fa-youtube"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
