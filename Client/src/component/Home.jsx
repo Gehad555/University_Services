7d0be968ed399c679b44122e030e8e9cb78d3e7b
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-10-04 14:42:54 +0300

    finish

--- Client/src/component/Home.jsx
+++ Client/src/component/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <div className={style.home} style={{ height: "100vh" }}>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary p-3">
           <div className="container-fluid" style={{ padding: "0 50px" }}>
             <img
               src="faculty-image-removebg-preview.png"
@@ -21,10 +21,10 @@ const Home = () => {
                 id={style.linkId}
                 className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-5 text-center"
               >
-                <Link>Home</Link>
-                <Link>About Us</Link>
-                <Link>About college</Link>
-                <Link>College Activities</Link>
+                <Link id={style.btn}>Home</Link>
+                <Link id={style.btn}>About Us</Link>
+                <Link id={style.btn}>About college</Link>
+                <Link id={style.btn}>College Activities</Link>
               </div>
               <div className={style.inputBox_container}>
                 <svg
@@ -88,8 +88,8 @@ const Home = () => {
             tenetur perspiciatis beatae itaque adipisci et facilis. lore
           </p>
           <div className="d-flex align-items-center justify-content-center gap-5 text-center">
-            <Link className="px-5">Login</Link>
-            <Link className="px-5">Register</Link>
+            <Link id={style.btn} className="px-5">Login</Link>
+            <Link id={style.btn} className="px-5">Register</Link>
           </div>
         </div>
       </div>
@@ -133,48 +133,48 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="d-flex border-top mt-5 border-black">
-          <div className="im">
-            <img src="faculty-image-removebg-preview.png" alt="" />
+        <div className="row mx-0 mt-5 align-items-center justify-content-between py-4" style={{ borderTop: '3px solid #57C6F2' }}>
+          <div className="col-3 d-flex justify-content-center">
+            <img src="faculty-image-removebg-preview.png" alt="" width='100px' />
           </div>
-          <div className="m">
-            <p>about us</p>
-            <div className="d">
-              <i className="fa-brands fa-linkedin"></i>
-              <p>(6) Ehab Yousef | LinkedIn</p>
+          <div className="col-4">
+            <p className="mb-0 fs-3" style={{ color: "#053B50" }}>About Us</p>
+            <div className="d-flex align-items-center gap-4 my-3">
+              <i className="fa-brands fa-linkedin fs-3"></i>
+              <Link to='/https://www.linkedin.com/in/ehabyousef/' id={style.profile} className="mb-1 text-black fs-5 position-relative">(6) Ehab Yousef | LinkedIn</Link>
             </div>
-            <div className="d">
-              <i className="fa-brands fa-linkedin"></i>
-              <p>(6) Ehab Yousef | LinkedIn</p>
+            <div className="d-flex align-items-center gap-4 my-3">
+              <i className="fa-brands fa-linkedin fs-3"></i>
+              <Link to='/https://www.linkedin.com/in/Sherry Ahmos /' id={style.profile} className="mb-1 text-black fs-5 position-relative">(6) Sherry Ahmos | LinkedIn</Link>
             </div>
-            <div className="d">
-              <i className="fa-brands fa-linkedin"></i>
-              <p>(6) Ehab Yousef | LinkedIn</p>
+            <div className="d-flex align-items-center gap-4 my-3">
+              <i className="fa-brands fa-linkedin fs-3"></i>
+              <Link to='/https://www.linkedin.com/in/Gehad Shalaby/' id={style.profile} className="mb-1 text-black fs-5 position-relative">(6) Gehad Shalaby | LinkedIn</Link>
             </div>
-            <div className="d">
-              <i className="fa-brands fa-linkedin"></i>
-              <p>(6) Ehab Yousef | LinkedIn</p>
+            <div className="d-flex align-items-center gap-4 my-3">
+              <i className="fa-brands fa-linkedin fs-3"></i>
+              <Link to='/https://www.linkedin.com/in/Yasmeen Fakhri El_Azazi/' id={style.profile} className="mb-1 text-black fs-5 position-relative">(8) Yasmeen Fakhri El_Azazi | LinkedIn</Link>
             </div>
           </div>
-          <div className="e">
-            <p>About College</p>
-            <div className="d">
-              <i className="fa-solid fa-phone"></i>
-              <p>(+2064) 3223007-(+2064) 32001258</p>
+          <div className="col-5">
+            <p className="mb-0 fs-3" style={{ color: "#053B50" }}>About College</p>
+            <div className="d-flex gap-3 align-items-center my-3">
+              <i className="fa-solid fa-phone fs-4"></i>
+              <p className="mb-1 fs-5">(+2064) 3223007-(+2064) 32001258</p>
             </div>
-            <div className="d">
-              <i className="fa-regular fa-envelope"></i>
-              <p>President-office@suez.edu.eg</p>
+            <div className="d-flex gap-3 align-items-center my-3">
+              <i className="fa-regular fa-envelope fs-4"></i>
+              <p className="mb-1 fs-5">President-office@suez.edu.eg</p>
             </div>
-            <div className="d">
-              <i className="fa-solid fa-location-dot"></i>
-              <p>4.5 Km the Ring Road, Ismailia, Egypt</p>
+            <div className="d-flex gap-3 align-items-center my-3">
+              <i className="fa-solid fa-location-dot fs-4"></i>
+              <p className="mb-1 fs-5">4.5 Km the Ring Road, Ismailia, Egypt</p>
             </div>
-            <div className="d">
-              <i className="fa-brands fa-facebook-f"></i>
-              <i className="fa-brands fa-instagram"></i>
-              <i className="fa-brands fa-x-twitter"></i>
-              <i className="fa-brands fa-youtube"></i>
+            <div className="d-flex gap-4 my-3">
+              <i className="fa-brands fa-facebook-f fs-4 "></i>
+              <i className="fa-brands fa-instagram fs-4 "></i>
+              <i className="fa-brands fa-x-twitter fs-4 "></i>
+              <i className="fa-brands fa-youtube fs-4 "></i>
             </div>
           </div>
         </div>
