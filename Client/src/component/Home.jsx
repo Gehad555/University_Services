a00e8c08fc4a9aa0660afe95bd31a943756dbb1b
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-10-04 14:51:55 +0300

    edit nav

--- Client/src/component/Home.jsx
+++ Client/src/component/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <div className={style.home} style={{ height: "100vh" }}>
         <nav className="navbar navbar-expand-lg bg-body-tertiary p-3">
-          <div className="container-fluid" style={{ padding: "0 50px" }}>
+          <div className="container-fluid" style={{ padding: "0 40px" }}>
             <img
               src="faculty-image-removebg-preview.png"
               alt=""
@@ -25,22 +25,22 @@ const Home = () => {
                 <Link id={style.btn}>About Us</Link>
                 <Link id={style.btn}>About college</Link>
                 <Link id={style.btn}>College Activities</Link>
-              </div>
-              <div className={style.inputBox_container}>
-                <svg
-                  className={style.search_icon}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                  alt="search icon"
-                >
-                  <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
-                </svg>
-                <input
-                  className={style.inputBox}
-                  id="inputBox"
-                  type="text"
-                  placeholder="Search..."
-                />
+                <div className={style.inputBox_container}>
+                  <svg
+                    className={style.search_icon}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 48 48"
+                    alt="search icon"
+                  >
+                    <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
+                  </svg>
+                  <input
+                    className={style.inputBox}
+                    id="inputBox"
+                    type="text"
+                    placeholder="Search..."
+                  />
+                </div>
               </div>
             </div>
           </div>
