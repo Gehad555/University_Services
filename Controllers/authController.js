04e23924eadd4608a791c861aabb5fd5e341ebc3
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2023-10-18 12:48:30 +0300

    Gmail
    Gmail

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -223,3 +223,4 @@ const postresetPasswordController = async(req,res)=>{
 }
 
 module.exports={loginController , registerController , forgotPasswordController ,getresetPasswordController,postresetPasswordController };
+
